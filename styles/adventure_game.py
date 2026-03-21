a465030b19c87acca811f7a4d5bbe322b4a2fa44
ChyBless1 <cblessingmuokwe@byupathway.edu>
2026-03-21 07:52:08 +0100

    complete overlay with css positioning

---
+++ styles/adventure_game.py
@@ -0,0 +1,51 @@
+# Author
+#chinwe Blessing Muokwe.
+# This program adventure program is based on my own original story idea about choosing between a Bible and Money
+# Title "hidden blessings"
+# I added multiple levels,several endings,and extra choices to make the game intresting and creative.
+
+print("Welcome to the Hidden Blessing Adventure Game!")
+print("Your father gives you a choice: BIBLE or MONEY.")
+# LEVEL 1
+choice1 = input ("Which do you choose? (BIBLE or MONEY): ")
+choice1 = choice1.lower()
+
+if choice1 == "bible":
+    print("You choose the Bible.")
+    print("Inside the Bible, you find a LETTER and some CASH: ")
+    # LEVEL 2 
+    choice2 = input("Which do you chose? (LETTER or CASH): ")
+    choice2 = choice2.lower()
+    if choice2 == "cash":
+        print("You spend it and it finishes. ")
+
+    elif choice2 == "letter":
+        print("choose the letter.")
+        print("The letter contains hidden directions to hidden blessings")
+    # Level 3
+        choice3 = input("Do you FOLLOW or IGNORE or PRAY? (FOLLOW, IGNORE, PRAY):")
+        choice3 = choice3.lower()
+        if choice3 == "follow":
+            print("You followed the directions.")
+            print("It leads you to a hidden mansion prepared for you.")
+
+        elif choice3 == "ignore":
+            print("You ignored the letter.")
+            print("You missed the hidden blessings.")
+
+        elif choice3 == "pray":
+            print("You chose to pray.")
+            print("You receive wisdom")
+        else:
+            print("Invalid choice! please FOLLOW or IGNORE or PRAY")
+        
+
+elif choice1 == "money":
+    print("You choose money.")
+    print("You spend and enjoy it for a short time.")
+
+else:
+    print("Invalid choice! please chose BIBLE or MONEY.")
+
+        
+

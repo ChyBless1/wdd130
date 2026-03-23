c257ee01790c764e045db64bb882d9e3e713a43f
ChyBless1 <cblessingmuokwe@byupathway.edu>
2026-03-23 16:25:35 +0100

    updated more choices, creativity to adventure game

--- styles/adventure_game.py
+++ styles/adventure_game.py
@@ -2,19 +2,20 @@
 #chinwe Blessing Muokwe.
 # This program adventure program is based on my own original story idea about choosing between a Bible and Money
 # Title "hidden blessings"
-# I added multiple levels,several endings,and extra choices to make the game intresting and creative.
+# For additional creativity, I added three levels,a three choice scenarios, and multiple endings to make the game more intresting and creative.
+# I also shared the game ideas to few person and improved it to make the choices clearer.
 
 print("Welcome to the Hidden Blessing Adventure Game!")
-print("Your father gives you a choice: BIBLE or MONEY.")
+print("Your father gives you a choice: BIBLE or MONEY or WALK-AWAY.")
 # LEVEL 1
-choice1 = input ("Which do you choose? (BIBLE or MONEY): ")
+choice1 = input ("Which do you choose? (BIBLE or MONEY or WALK-AWAY): ")
 choice1 = choice1.lower()
 
 if choice1 == "bible":
     print("You choose the Bible.")
     print("Inside the Bible, you find a LETTER and some CASH: ")
     # LEVEL 2 
-    choice2 = input("Which do you chose? (LETTER or CASH): ")
+    choice2 = input("Which do you chose? (LETTER, CASH or SHARE): ")
     choice2 = choice2.lower()
     if choice2 == "cash":
         print("You spend it and it finishes. ")
@@ -38,11 +39,22 @@ if choice1 == "bible":
             print("You receive wisdom")
         else:
             print("Invalid choice! please FOLLOW or IGNORE or PRAY")
+
+    elif choice2== "share":
+     print("You choose to share what you found with others.")
+     print("Your kindness leads to greater and unexpected blessings.")
+
+    else:
+     print("Invalid choice! please choose LETTER, CASH or SHARE")
         
 
 elif choice1 == "money":
     print("You choose money.")
     print("You spend and enjoy it for a short time.")
+
+elif choice1 == "walk-away":
+   print("You decide to walk away.")
+   print("Later, you realize that you may have missed a life-changing blessing")
 
 else:
     print("Invalid choice! please chose BIBLE or MONEY.")
